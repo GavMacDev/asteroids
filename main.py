e7a2691980e1db6045b66b7e18283826c56b0a7f
GavMacDev <macdonaldg22@gmail.com>
2024-10-10 19:08:21 -0500

    Added floating asteroids

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import pygame
 # import everything from constants.py
 from constants import *
 from player import Player
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
@@ -15,6 +17,13 @@ def main():
     
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
     Player.containers = (updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
